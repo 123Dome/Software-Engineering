66cbdfb8e2c7a5962f3714411fbb50e0d8c4e9cc
Dome <dominik.puck@web.de>
2024-10-30 20:59:43 +0100

    Revert "Calculate Priority fürs testen"
    This reverts commit 46bcdcc2369c158f8dacda758c58de6bb13dcc98.

--- src/org/hbrs/se1/ws24/exercises/uebung4/UserStory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/UserStory.java
@@ -85,20 +85,11 @@ public class UserStory implements Serializable {
     }
 
     /**
-     * Berechnet die Priorität dynamisch basierend auf den Attributen.
-     *
-     * @return die berechnete Priorität der User Story
-     */
-    public double calculatePriority() {
-        return (double) (value + penalty) / (risk + effort);
-    }
-
-    /**
      * Gibt die berechnete Priorität der User Story zurück.
      *
      * @return die Priorität der User Story
      */
     public double getPriority() {
-        return this.calculatePriority();
+        return this.priority;
     }
 }
