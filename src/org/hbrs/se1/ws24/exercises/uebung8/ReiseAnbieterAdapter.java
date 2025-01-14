b9176de953c92c7b694bda3ac8a12835cf45eb89
Dome <dominik.puck@web.de>
2025-01-14 23:53:36 +0100

    Übung 10

--- src/org/hbrs/se1/ws24/exercises/uebung8/ReiseAnbieterAdapter.java
+++ src/org/hbrs/se1/ws24/exercises/uebung8/ReiseAnbieterAdapter.java
@@ -2,10 +2,7 @@ package org.hbrs.se1.ws24.exercises.uebung8;
 
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-
+/*
 
 //    BEISPIELKLASSE
 public class ReiseAnbieterAdapter {
@@ -38,3 +35,4 @@ public class ReiseAnbieterAdapter {
         return ergebnisse;
     }
 }
+*/
